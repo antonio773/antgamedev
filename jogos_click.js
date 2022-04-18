0075079cd9ea33a86ec7f66d6201cf6f282ef316
antonio773 <77169536+antonio773@users.noreply.github.com>
2022-04-18 14:26:19 -0300

    Add files via upload

---
+++ jogos_click.js
@@ -0,0 +1,12 @@
+function jogo_click(){
+  console.log("jogos");
+}
+ function ini_click(){
+   window.open("index.html");
+ }
+ function youtube_click(){
+   window.open("https://youtube.com/channel/UCtYajtzQNrdLsYL19VXZppQ")
+ }
+function contato_click(){
+  window.open("contato.html");
+}
---
+++ script.js
@@ -0,0 +1,13 @@
+function ini_click(){
+  console.log("inicio")
+}
+function jogo_click(){
+ window.open("jogos.html");
+ window.close();
+}
+function youtube_click(){
+window.open("https://youtube.com/channel/UCtYajtzQNrdLsYL19VXZppQ");
+}
+function contato_click(){
+  window.open("contato.html");
+}
